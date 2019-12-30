596700f87c6ebeeec1ccf55c13141d3d4535c38c
idanO <iojbemail@gmail.com>
2019-12-30 20:49:04 +0200

    project a

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core"
+
+const users = ["silvia", "adir", "sivan"];
+@Component({
+    selector: "app-header",
+    templateUrl: "./header.component.html",
+    styleUrls: ["./header.component.css"]
+})
+export class AppHeader {
+    public title: string;
+    public clicks: number
+    constructor() {
+        this.title = "Store App";
+        this.clicks = 10
+    }
+
+    changeHeader = (newHeader: string) => {
+        this.title = newHeader
+    }
+}
