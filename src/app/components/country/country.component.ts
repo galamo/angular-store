3dafdba31c02c325bea8abc67ae06b4a58f67579
idanO <iojbemail@gmail.com>
2020-01-09 19:14:22 +0200

    c-component

---
+++ src/app/components/country/country.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-country',
+    templateUrl: './country.component.html',
+    styleUrls: ['./country.component.css']
+})
+export class CountryComponent implements OnInit {
+    @Input('country-object') countryObj: any
+
+    constructor() { console.log(this.countryObj, "constructor") }
+
+    ngOnInit() {
+        console.log(this.countryObj, "ngOnInit")
+    }
+
+}
